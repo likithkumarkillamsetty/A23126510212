8a46a1d50d0d2ee68da9231efb6f61e26aa15c18
likithkumarkillamsetty <killamsettylikithkumar@gmail.com>
2026-06-23 11:35:03 +0530

    added logger with auth token

--- VechicleMaintenanceMicroservice/src/main/java/com/likith/VechicleMaintenanceMicroservice/middleware/Logger.java
+++ VechicleMaintenanceMicroservice/src/main/java/com/likith/VechicleMaintenanceMicroservice/middleware/Logger.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Logger {
 
     private static final String LOG_URL = "http://20.244.56.144/evaluation-service/logs";
-    private static String authToken = "";
+    private static String authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJraWxsYW1zZXR0eWxpa2l0aGt1bWFyLjIzLmNzZUBhbml0cy5lZHUuaW4iLCJleHAiOjE3ODIxOTM0NDIsImlhdCI6MTc4MjE5MjU0MiwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjQzMGQyNDNlLTIwYWItNDc0OS05ZDJkLTY4NjgyNzdkNmViOCIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6Imxpa2l0aCBrdW1hciBraWxsYW1zZXR0eSIsInN1YiI6IjAzZTZmYTZhLTg3ZmEtNGJlMy05NDA3LWM1YjE5YmZkNzQ2ZiJ9LCJlbWFpbCI6ImtpbGxhbXNldHR5bGlraXRoa3VtYXIuMjMuY3NlQGFuaXRzLmVkdS5pbiIsIm5hbWUiOiJsaWtpdGgga3VtYXIga2lsbGFtc2V0dHkiLCJyb2xsTm8iOiJhMjMxMjY1MTAyMTIiLCJhY2Nlc3NDb2RlIjoiTVRxeGFyIiwiY2xpZW50SUQiOiIwM2U2ZmE2YS04N2ZhLTRiZTMtOTQwNy1jNWIxOWJmZDc0NmYiLCJjbGllbnRTZWNyZXQiOiJXR0h0a3F1dVZXdm1CeFlLIn0.efq4SNlqvdL1v-nnQn-O9l-GYsgy54o_auJsM4uYk6s";
 
     public static void setToken(String token) {
         authToken = token;
